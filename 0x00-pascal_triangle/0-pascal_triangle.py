07152de5c90bc76d55f7e414080d065ef65ec2f8
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-11-30 08:47:55 +0000

    Added a Pascals triangle module

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """Pascals Triangle"""
+    array = []
+    for i in range(n):
+        temp = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp.append(1)
+            else:
+                temp.append(array[i-1][j-1] + array[i-1][j])
+        array.append(temp)
+    return array
